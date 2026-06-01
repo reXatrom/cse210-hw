8cf614ff346360f7289d3742c84b3c97a9b57ef5
reXatrom <ajstylejoshua2837@gmail.com>
2026-06-01 21:47:03 +0100

    Finished Week05 : Homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        // Math Assignment
+        MathAssignment math = new MathAssignment(
+            "Jane Doe",
+            "Fractions",
+            "Section 1.3",
+            "1-10, 15-20");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+
+
+        // Writing Assignment
+        WritingAssignment writing = new WritingAssignment(
+            "John Smith",
+            "European History",
+            "The Causes of World War II");
+
+
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
