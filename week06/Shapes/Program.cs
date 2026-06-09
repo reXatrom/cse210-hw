84a16eeb8b990b2606a99bdbf8e9812020692571
reXatrom <ajstylejoshua2837@gmail.com>
2026-06-09 14:06:27 +0100

    Finished W06: Shapes

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Red", 5));
+        shapes.Add(new Square("Blue", 4));
+        shapes.Add(new Rectangle("Green", 6, 8));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
