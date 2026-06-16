ff44ecdd70187a0cf888589a52aaba3aeeab0839
reXatrom <ajstylejoshua2837@gmail.com>
2026-06-16 21:10:47 +0100

    Exercise Tracking Program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new RunningActivity("03 Nov 2022", 30, 3.0));
+        activities.Add(new CyclingActivity("04 Nov 2022", 45, 12.0));
+        activities.Add(new SwimmingActivity("05 Nov 2022", 40, 40));
+
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
